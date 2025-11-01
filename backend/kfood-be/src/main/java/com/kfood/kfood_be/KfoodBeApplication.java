29220e5f5e455281f17a469d6bae687b5b8ce659
kimjunwoo <popple1101@naver.com>
2025-11-01 15:16:05 +0900

    백엔드 폴더 생성

---
+++ backend/kfood-be/src/main/java/com/kfood/kfood_be/KfoodBeApplication.java
@@ -0,0 +1,13 @@
+package com.kfood.kfood_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KfoodBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KfoodBeApplication.class, args);
+	}
+
+}
---
+++ backend/kfood-be/src/test/java/com/kfood/kfood_be/KfoodBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kfood.kfood_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KfoodBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
