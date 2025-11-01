be0fb9c6a779b4a544171c921908d2def788ea1e
believeme <cain1227@naver.com>
2025-11-02 01:59:04 +0900

    fix: DTO수정

--- backend/kfood-be/src/main/java/com/kfood/kfood_be/youtube/dto/YoutubeVideoResponseDto.java
+++ backend/kfood-be/src/main/java/com/kfood/kfood_be/youtube/dto/YoutubeVideoResponseDto.java
@@ -1,10 +1,10 @@
 package com.kfood.kfood_be.youtube.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Data
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class YoutubeVideoResponseDto {
